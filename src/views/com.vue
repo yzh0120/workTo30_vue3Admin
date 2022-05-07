62047fa18521cef9f40b55595d23cac8e2caf89c
yzh0120 <644193385@qq.com>
2022-05-07 23:54:28 +0800

    up

---
+++ src/views/com.vue
@@ -0,0 +1,27 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import {
+  onMounted,
+  computed,
+  watch,
+  getCurrentInstance,
+  reactive,
+  toRefs,
+} from "vue";
+export default {
+  setup() {
+    let data = reactive({
+      demo: 1,
+    });
+    return {
+      ...toRefs(data),
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
